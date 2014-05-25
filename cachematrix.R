5d3519f88a176d1ec16075d1163c8d589c87aef9
Elena <Streega@users.noreply.github.com>
2014-05-26 02:36:56 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,15 @@
 ## inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-    c <- NULL
-    set <- function(b) {
-        x <<- b
-        c <<- NULL
+    c <- NULL               ## Initialize value of inverse matrix 'c' 
+    set <- function(b) {    ## Set value of matrix from user input 'b'
+        x <<- b             ## Set to global 'x' value of matrix input 'b'
+        c <<- NULL          ## Clear inverse matrix 'c' value if it exists
     }
-    get <- function() x
-    setinv <- function(solve) c <<- solve
-    getinv <- function() c
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+    get <- function() x     ## Get value of matrix if it has been set
+    setinv <- function(solve) c <<- solve ## Set value of the inverse of matrix
+    getinv <- function() c  ## Get value of the inverse matrix 'c'
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 ## Function cacheSolve computes the inverse of the special "matrix" returned by
@@ -21,15 +19,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## from the cache.
 
 cacheSolve <- function(x, ...) {
-    ptm <- proc.time()       ## Return a matrix that is the inverse of 'x'
-    c <- x$getinv()          ## Get Matrix "c" from cache
-    if(!is.null(c)) {        ## Check if not null, return value read from cache
+    ptm <- proc.time()       
+    c <- x$getinv()          ## Get inverse matrix 'c' from cache
+    if(!is.null(c)) {        ## Check if not NULL, return value read from cache
         cat("Process Time", proc.time() - ptm)
         return(c)
     }
-    mydata <- x$get()        ## If "c" is NULL, get origal Matrix
+    mydata <- x$get()        ## If inverse matrix 'c' is NULL, get origal matrix
     c <- solve(mydata, ...)  ## Calculate inverse using solve
     x$setinv(c)
     cat("Process Time", proc.time() - ptm)
-    c                        ## Returm inverse
+    c                        ## Returm inverse matrix 'c'
 }
